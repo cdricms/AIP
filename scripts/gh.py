61bb578bf9d53fec9587fa08ee78ec03a6887539
Smoqu <cedric.mas.83@gmail.com>
2020-12-21 17:09:50 +0100

    import fixed

--- scripts/gh.py
+++ scripts/gh.py
@@ -4,7 +4,7 @@ import os
 import sys
 import github
 
-from settings import execute_commands
+from . import settings
 
 
 load_dotenv(find_dotenv())
@@ -21,7 +21,7 @@ def repo_create(foldername, private):
         'git commit -m "Initial commit"',
         'git push -u origin master']
                 
-    execute_commands(commands)
+    settings.execute_commands(commands)
 
     link = f"https://github.com/{login}/{repo.name}"
     os.system(f"echo {foldername} commited @ {link}")
